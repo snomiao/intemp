15f3bd554af3b3092c68985cbf73886b8cb86076
snomiao <snomiao@gmail.com>
2025-03-13 09:00:05 +0000

    fix(main): myip
    myip

--- index.spec.ts
+++ index.spec.ts
@@ -2,21 +2,28 @@ import { expect, it } from "bun:test";
 import { readFile, rm } from "fs/promises";
 import intemp from "./index.ts";
 
-it("convert correctly", async () => {
-  await rm("./config.yaml").catch(() => null);
-  // const intemp = (await import(".")).default;
-  await intemp();
+it("convert web data correctly", async () => {
+  const dir = "tests/web";
+  await rm(dir + "/myip.txt").catch(() => null);
+  await intemp({ dir });
 
-  expect(await readFile("./config.yaml", "utf8")).toEqual(
-    await readFile("./config.expected.yaml", "utf8")
+  const myip = await (await fetch("https://ifconfig.me/ip")).text();
+  console.log(myip)
+  expect(await readFile(dir + "/myip.txt", "utf8")).toEqual(
+    myip
+  );
+});
+it("convert env correctly", async () => {
+  const dir = "tests/env";
+  await rm(dir + "/config.yaml").catch(() => null);
+  await intemp({ dir });
+
+  expect(await readFile(dir + "/config.yaml", "utf8")).toEqual(
+    await readFile(dir + "/config.expected.yaml", "utf8")
   );
 });
 it("throw when missing env", async () => {
-  await rm("./tests/missing/config.yaml").catch(() => null);
-  // const intemp = (await import(".")).default;
-  await intemp({ dir: "./tests/missing" });
-
-  expect(await readFile("./tests/missing/config.yaml", "utf8")).toEqual(
-    await readFile("./tests/missing/config.expected.yaml", "utf8")
-  );
+  const dir = "tests/missing";
+  await rm("/config.yaml").catch(() => null);
+  expect(() => intemp({ dir })).toThrowError(Error);
 });
